ca1df7d610074896554eb65fd6430863b8040ee9
GabrielAlgo12 <125403179+GabrielAlgo12@users.noreply.github.com>
2024-01-10 17:27:25 -0500

    Update main.py

--- Plantilla/main.py
+++ Plantilla/main.py
@@ -10,7 +10,7 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 @bot.command()
 async def meme(ctx):
         # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
-        imagen = random.choice(os.listdir(f"meme_bot/images"))
+        imagen = random.choice(os.listdir("meme_bot/images"))
         with open(f"meme_bot/images/{imagen}","rb") as f:
             meme = discord.File(f)
             # A continuación, podemos enviar este archivo como parámetro.
